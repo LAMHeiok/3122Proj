7ebb2abd1d134ffbbce8bf9680afe3092d496ed5
LAM Ching Hei <22087632d@connect.polyu.hk>
2025-04-06 21:49:29 +0800

    update: delete of unecessary older versions

--- Feb 4 old version/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Specify the packages to install
-packages = ['openai', 'streamlit']
-
-# Install each package using pip
-for package in packages:
-    os.system(f'pip install {package}')
--- Feb 4 old version/welcomepage.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Welcome to Writing Assessment system")
-st.subheader("subheader test")
-
-if st.button("Get Started"):
-    st.switch_page("test.py")
